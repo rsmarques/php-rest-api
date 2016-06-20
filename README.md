e5855962563d632738393159a37d4d1157a4ade4
Ricardo Marques <ricardo.ds.marques@gmail.com>
2016-06-20 01:26:40 +0000

    added unit tests

--- README.md
+++ README.md
@@ -15,5 +15,6 @@ In order to build the application a PHP web server is needed (e.g., [Apache](htt
 
 1. Configure your Firebase credentials in /app/config/firebase.php
 2. Define your models/controllers
-3. Application URL usage: /{modelName}⁄{id?}, according to [REST architecture](https://en.wikipedia.org/wiki/Representational_state_transfer)
-4. You can test a running version [here](http://php-assessment.herokuapp.com)
+3. composer install (for unit testing)
+4. Application URL usage: /{modelName}⁄{id?}, according to [REST architecture](https://en.wikipedia.org/wiki/Representational_state_transfer)
+5. You can test a running version [here](http://php-assessment.herokuapp.com)
---
+++ tests/ContactsTest.php
@@ -0,0 +1,82 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ContactsTest extends TestCase
+{
+    protected $client;
+
+    protected function setUp()
+    {
+        $this->client   = new GuzzleHttp\Client([
+            'base_uri' => 'http://localhost'
+        ]);
+    }
+
+    public $testData    = [
+        "57673e7a02243" => ['name' => 'Michal', 'phone_number' => '506088156',  'address' => 'Michalowskiego 41'],
+        "57673e7a8e093" => ['name' => 'Marcin', 'phone_number' => '502145785',  'address' => 'Opata Rybickiego 1'],
+        "57673e7b2a6ef" => ['name' => 'Piotr',  'phone_number' => '504212369',  'address' => 'Horacego 23'],
+        "57673e7bb8cbf" => ['name' => 'Albert', 'phone_number' => '605458547',  'address' => 'Jan Pawla 67'],
+    ];
+
+    public function test()
+    {
+        // Testing delete all
+        $response   = $this->client->delete('/contact', []);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        // Testing creating a list of items
+        $response   = $this->client->put('/contact', ['form_params' => $this->testData]);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $data       = json_decode($response->getBody(), true);
+        $this->assertEquals($data, $this->testData);
+
+        // Testing retrieving an item (first element)
+        reset($this->testData);
+        $response   = $this->client->get('/contact/' . key($this->testData), []);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $data       = json_decode($response->getBody(), true);
+        $this->assertEquals($data, current($this->testData));
+
+        // Testing retrieving all items
+        $response   = $this->client->get('/contact/', []);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $data       = json_decode($response->getBody(), true);
+        $this->assertEquals($data, $this->testData);
+
+        // Testing deleting one element (last element)
+        end($this->testData);
+        $response   = $this->client->delete('/contact/' . key($this->testData), []);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $response   = $this->client->get('/contact/' . key($this->testData), []);
+        $data       = json_decode($response->getBody(), true);
+        // assuring the item is no longer in the db
+        $this->assertEquals($data, []);
+
+        // Testing updating one element (last element into first)
+        reset($this->testData);
+        $response   = $this->client->put('/contact/' . key($this->testData), ['form_params' => end($this->testData)]);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $data       = json_decode($response->getBody(), true);
+        $this->assertEquals($data, current($this->testData));
+
+        // Testing creating an element
+        $response   = $this->client->post('/contact/', ['form_params' => reset($this->testData)]);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $data       = json_decode($response->getBody(), true);
+        $this->assertEquals($data, current($this->testData));
+
+        $response   = $this->client->get('/contact/', []);
+        $data       = json_decode($response->getBody(), true);
+        // assuring last item is equal than the one created
+        $this->assertEquals(end($data), current($this->testData));
+
+    }
+}

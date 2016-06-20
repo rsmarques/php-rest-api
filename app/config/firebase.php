6876135e40a6c1b0ac95bb4d6e7c5cce536d8ed1
Ricardo Marques <ricardo.ds.marques@gmail.com>
2016-06-20 02:21:19 +0000

    added local firebase varrs

--- app/config/firebase.php
+++ app/config/firebase.php
@@ -1,8 +1,8 @@
 <?php
 
 $firebaseConfig = array(
-    'url'       => '', // configure your firebase URL here
-    'token'     => '', // configure your firebase token here
+    'url'       => 'https://php-assessment.firebaseio.com/', // configure your firebase URL here
+    'token'     => 'H63Xnwr0aXXVtq1Cbfhuk5vATbmMFwUrhMb8N8fu', // configure your firebase token here
 );
 
 return $firebaseConfig;
